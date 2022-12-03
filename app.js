79f31b34206481ddb350e12467a150e9c49dd2c7
Young <syoh0313@yonsei.ac.kr>
2022-12-03 17:28:23 +0900

    feat : 1차 팀플

--- app.js
+++ app.js
@@ -22,73 +22,7 @@ conn.connect();
 app.use(bodyParser.json())
 
 
-//id에 AUTHOR 넣으면 모든 row 가져올 수 있음
-// app.get('/:id', (req, res)=>{
-//   const values = req.params.id.toString()
-//   var sql = 'SELECT * FROM '+values
-  
-//   conn.query(sql ,(err,row,fields)=>{
-//     res.send(row)
-//     //res.send(fields);
-//   })
-// });
-
-
-
-//AUTHOR 테이블에 info row 추가
-// const info ={
-//   "id" : 11,
-//   "name" : "db",
-//   "profile" : "tester"
-// }
-// var sql = 'INSERT INTO AUTHOR(id,name,profile) VALUES(?,?,?)';
-// var params = [info["id"], info["name"], info["profile"]]
-// conn.query(sql, params, (err, rows, fields)=>{
-//   if(err){
-//     console.log(err)
-//   }else{
-//     console.log(rows)
-//   }
-// })
-
-// body 받아서 db에 추가
-// app.post('/post', (req, res)=>{
-//   console.log("통신")
-//   console.log(req.body)
-  
-//   const {id, name, profile} = req.body
-    
-//   var sql = 'INSERT INTO AUTHOR(id,name,profile) VALUES(?,?,?)';
-//   var params = [id, name, profile]
-//   conn.query(sql, params, (err, rows, fields)=>{
-//     if(err){
-//       res.send(err)
-//     }else{
-//       res.send(rows)
-//       console.log("success")
-//     }
-//   })
-// })
-
-
-// 대표속성 업데이트
-app.post('/post/attr', (req, res)=>{
-  const {attr} = req.body
-  console.log(attr)
-  var sql = 'UPDATE attr_scan SET head_attr="'+attr+'" where attr_name="나이"';
-  var params = attr
-  conn.query(sql, params, (err, rows, fields)=>{
-    if(err){
-      res.send(err)
-      console.log(err)
-    }else{
-      res.send(rows)
-      console.log("success")
-    }
-  })
-})
-
-// 사용자 추가 대표속성 업데이트
+// 사용자가 추가한 속성 att dic 에 추가
 app.post('/post/attr/dic', (req, res)=>{
   const {id, attr} = req.body
   var sql = 'INSERT INTO attr_dic(id,attr) VALUES(?,?)';
@@ -104,11 +38,46 @@ app.post('/post/attr/dic', (req, res)=>{
   })
 })
 
-// 표준결합키 업데이트
-app.post('/post/key', (req, res)=>{
-  const {key} = req.body
-  var sql = 'UPDATE attr_scan SET head_key="'+key+'" where attr_name="나이"';
-  var params = key
+// 사용자가 추가한 키 key dic 에 추가
+app.post('/post/key/dic', (req, res)=>{
+  const {id, key} = req.body
+  console.log("444444", id, key)
+  var sql = 'INSERT INTO key_dic(id, key_attr) VALUES(?,?)';
+  var params = [id, key]
+  conn.query(sql, params, (err, rows, fields)=>{
+    if(err){
+      res.send(err)
+      console.log(err)
+    }else{
+      res.send(rows)
+      console.log("success")
+    }
+  })
+})
+
+// scan table에 대표속성 업데이트
+app.put('/put/attr', (req, res)=>{
+  const {attr, name} = req.body
+  console.log(attr, name)
+  var sql = 'UPDATE attr_scan SET head_attr=(?) where attr_name=(?)';
+  var params = [attr, name]
+  conn.query(sql, params, (err, rows, fields)=>{
+    if(err){
+      res.send(err)
+      console.log(err)
+    }else{
+      res.send(rows)
+      console.log("success")
+    }
+  })
+})
+
+
+// scan table에 표준결합키 업데이트
+app.put('/put/key', (req, res)=>{
+  const {key, name} = req.body
+  var sql = 'UPDATE attr_scan SET head_key=(?) where attr_name=(?)';
+  var params = [key, name]
   conn.query(sql, params, (err, rows, fields)=>{
     if(err){
       res.send(err)
@@ -122,33 +91,26 @@ app.post('/post/key', (req, res)=>{
 
 //대표속성 사전 값 불러오기
 app.get('/attr/dic', (req, res)=>{
-    // const values = req.params.id.toString()
     var sql = 'SELECT * FROM attr_dic'
-    
     conn.query(sql ,(err,row,fields)=>{
       res.send(row)
-      //res.send(fields);
     })
   });
 
+
 //표준결합키 사전 값 불러오기
 app.get('/key/dic', (req, res)=>{
-  // const values = req.params.id.toString()
   var sql = 'SELECT * FROM key_dic'
-  
   conn.query(sql ,(err,row,fields)=>{
     res.send(row)
-    //res.send(fields);
   })
 });
 
-//스캔 값 불러오기
+//scan table 값 불러오기
 app.get('/scan', (req, res)=>{
-  // const values = req.params.id.toString()
   var sql = 'SELECT * FROM attr_scan'
   
   conn.query(sql ,(err,row,fields)=>{
     res.send(row)
-    //res.send(fields);
   })
 });
