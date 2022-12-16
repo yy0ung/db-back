0f997cfa844e1dd88dd1d0db8b8770bd4d01f4b6
Jeongseop <kbp1021@naver.com>
2022-12-16 14:22:25 +0900

    초기 테이블 선택 table 생성 (속성 제외하고 만듬)

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 const server = app.listen(3000, ()=>{console.log('start server');});
 
-//connect db
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const maria = require('mysql');
@@ -18,17 +17,38 @@ const conn = maria.createConnection({
   password:conf.password,
   database:conf.database
 });
-
 conn.connect();
+
+var select_table = require('./select_table');
+select_table.create();
 
 var category = require('./category');
 category.create('1_fitness_measurement');
+
 var statistic = require('./statistic');
 statistic.create('1_fitness_measurement');
 
 app.get('/api/table',(req,res) => {
   conn.query(
-    "SELECT table_name 테이블명 FROM information_schema.tables WHERE table_schema= 'dbproject'",
+    "SELECT * FROM selecttable",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  )
+});
+
+app.get('/api/table/1_fitness_measurement/category',(req,res) => {
+  conn.query(
+    "SELECT * FROM category_attribute",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  )
+});
+
+app.get('/api/table/1_fitness_measurement/statistic',(req,res) => {
+  conn.query(
+    "SELECT * FROM statistic_attribute",
     (err, rows, fields) => {
       res.send(rows);
     }
--- category.js
+++ category.js
@@ -7,25 +7,25 @@ category.create = function (tablename){
         conn.query(sql, function (err, result) {
             if (err) throw err;
             console.log("Table created");
-          });
-          var sql = "insert into category_attribute(속성명, 데이터_타입) select column_name, data_type from INFORMATION_SCHEMA.COLUMNS where table_name='"+tablename+"' and data_type='int'";
-          conn.query(sql, function (err, result) {
+        });
+        var sql = "insert into category_attribute(속성명, 데이터_타입) select column_name, data_type from INFORMATION_SCHEMA.COLUMNS where table_name='"+tablename+"' and data_type='int'";
+        conn.query(sql, function (err, result) {
             if (err) throw err;
             console.log("Table created");
-          });
-          conn.query("select 속성명 from category_attribute", function(err, row, fields){
+        });
+        conn.query("select 속성명 from category_attribute", function(err, row, fields){
             for(var i=0; i<row.length;i++){
-              conn.query("UPDATE category_attribute SET NULL_레코드_수 = (SELECT COUNT(*) FROM "+tablename+" WHERE "+row[i].속성명+" IS NULL) WHERE 속성명='"+row[i].속성명+"'");
-              conn.query("UPDATE category_attribute SET NULL_레코드_비율 = NULL_레코드_수/(SELECT COUNT(*) FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
-              conn.query("UPDATE category_attribute SET 상이_수치_값 = (SELECT COUNT(DISTINCT "+row[i].속성명+") FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
-              conn.query("UPDATE category_attribute SET 최대_값 = (SELECT MAX("+row[i].속성명+") FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
-              conn.query("UPDATE category_attribute SET 최소_값 = (SELECT MIN("+row[i].속성명+") FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
-              conn.query("UPDATE category_attribute SET 0_레코드_수 = (SELECT COUNT(*) FROM "+tablename+" WHERE "+row[i].속성명+"=0) WHERE 속성명='"+row[i].속성명+"'");
-              conn.query("UPDATE category_attribute SET 0_레코드_비율 = 0_레코드_수/(SELECT COUNT(*) FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
+                conn.query("UPDATE category_attribute SET NULL_레코드_수 = (SELECT COUNT(*) FROM "+tablename+" WHERE "+row[i].속성명+" IS NULL) WHERE 속성명='"+row[i].속성명+"'");
+                conn.query("UPDATE category_attribute SET NULL_레코드_비율 = NULL_레코드_수/(SELECT COUNT(*) FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
+                conn.query("UPDATE category_attribute SET 상이_수치_값 = (SELECT COUNT(DISTINCT "+row[i].속성명+") FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
+                conn.query("UPDATE category_attribute SET 최대_값 = (SELECT MAX("+row[i].속성명+") FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
+                conn.query("UPDATE category_attribute SET 최소_값 = (SELECT MIN("+row[i].속성명+") FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
+                conn.query("UPDATE category_attribute SET 0_레코드_수 = (SELECT COUNT(*) FROM "+tablename+" WHERE "+row[i].속성명+"=0) WHERE 속성명='"+row[i].속성명+"'");
+                conn.query("UPDATE category_attribute SET 0_레코드_비율 = 0_레코드_수/(SELECT COUNT(*) FROM "+tablename+") WHERE 속성명='"+row[i].속성명+"'");
             }
-          });
+        });
         return ;
     });
-  }
+}
 
 module.exports = category;
