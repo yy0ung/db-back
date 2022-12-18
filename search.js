988da2f6a0528f29bcb62c35a85a9c07b49eb242
Jeongseop <kbp1021@naver.com>
2022-12-18 15:06:20 +0900

    +주석

--- search.js
+++ search.js
@@ -1,6 +1,8 @@
 var search = {};
 
+//검색기능
 search.create = function (tablename, key, att, attName){
+    //인자값이 들어오지 않는 경우에는 0을 해당 값에 넣어주고 scantable에 있는 모든 레코드들을 표시한다.
     var sql = "select * from scantable";
     if (tablename==0 && key==0 && att==0 && attName==0)
         var sql = "select * from scantable";
@@ -33,7 +35,7 @@ search.create = function (tablename, key, att, attName){
     else if(attName==0)
         var sql = "SELECT * FROM scantable WHERE 대표_속성='"+att+"' and 테이블_명='"+tablename+"' and 대표_결합키='"+key;
     else
-        var sql = "select * from scantable";
+    var sql = "SELECT * FROM scantable WHERE 테이블_명=(SELECT table_name FROM information_schema.COLUMNS WHERE COLUMN_NAME='"+attName+"') and 테이블_명='"+tablename+"' and 대표_결합키='"+key+"' and 대표_속성='"+att;
     return sql;
 }
 
