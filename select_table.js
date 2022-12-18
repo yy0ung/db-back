ae4ea90e45a87261df016038131b81b631b8a5f8
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2022-12-19 02:34:39 +0900

    fileName 값을 이용하도록 했고, insert대신 update를 사용하도록 했습니다.

--- select_table.js
+++ select_table.js
@@ -1,7 +1,7 @@
 var con = require('./connect');
 var select_table = {};
 
-select_table.create = function (){
+select_table.create = function (fileName){ ////   fileName을 이용하도록 추가
     con.getConnection(function(err,conn){
         var sql = "create table if not exists selecttable(테이블_명 VARCHAR(255), 레코드_수 int, 속성 VARCHAR(255), 스캔여부 boolean);";
         conn.query(sql, function (err, result) {
@@ -14,8 +14,8 @@ select_table.create = function (){
           console.log("Table created");
         });
         
-        //속성 update 필요
-        var sql = "insert into selecttable(속성) SELECT group_concat(COLUMN_NAME) FROM information_schema.columns WHERE TABLE_NAME = '"+fileName+"'";
+        //속성 update 필요   -> insert문에서 update문으로 바꿈.
+        var sql = "update selecttable set 속성= (SELECT group_concat(COLUMN_NAME) FROM information_schema.columns) WHERE TABLE_NAME = '"+fileName+"'";
         conn.query(sql, function (err, result) {
           if (err) throw err;
           console.log("Table created");
