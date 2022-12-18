4a547239e6a3ee3bf891b0257d7c4f39e566bb8d
Jeongseop <kbp1021@naver.com>
2022-12-18 15:01:02 +0900

    검색기능 추가

--- app.js
+++ app.js
@@ -31,6 +31,19 @@ statistic.create('1_fitness_measurement');
 var scan_table = require('./scan_table');
 scan_table.create('1_fitness_measurement');
 
+var search = require('./search');
+
+//선택 가능한 테이블 목록
+//대표속성은 하나만 입력되어 있을 경우 검색 가능
+app.get('/api/search',(req,res) => {
+  //search.create에 순서대로 테이블명,표준결합키,대표속성,속성명 을 인자로 받고 해당하는 레코드(scantable의)를 반환. 여기서 레코드는 선택 가능한 테이블 목록의 레코드를 의미
+  conn.query(search.create('2_physical_instructor_practice_info',0,0,'TEL_NO'), function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+})
+
+//현재 등록된 테이블들 전부 표시
 app.get('/api/table',(req,res) => {
   conn.query(
     "SELECT * FROM selecttable",
@@ -40,6 +53,7 @@ app.get('/api/table',(req,res) => {
   )
 });
 
+//1_fitness_measurement의 범주 속성 목록
 app.get('/api/table/1_fitness_measurement/category',(req,res) => {
   conn.query(
     "SELECT * FROM category_attribute",
@@ -49,6 +63,7 @@ app.get('/api/table/1_fitness_measurement/category',(req,res) => {
   )
 });
 
+//수치 속성 목록
 app.get('/api/table/1_fitness_measurement/statistic',(req,res) => {
   conn.query(
     "SELECT * FROM statistic_attribute",
