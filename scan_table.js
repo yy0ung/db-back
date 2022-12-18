2d58e8fa627f0bd2ac86da2c63d0344e11c6261e
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2022-12-18 21:25:37 +0900

    sql문으로 속성, 결합키 처리하도록 함.    ','로 data들 나열함.

--- scan_table.js
+++ scan_table.js
@@ -14,33 +14,9 @@ scan_table.create = function (){
           console.log("Table created");
         });
         //대표_속성, 대표_결합키 SQL문 필요 //기본 값이 null이라면...
-        var sqlForStatistic = "select 대표_속성, 대표_결합키  from statistic_attribute";
-        var sqlForCategory = "select 대표_속성, 대표_결합키 from category_attribute";
-        var primaryProperties='';
-        var primaryKey='';
-        conn.query(sqlForStatistic, function (err, result) {
-          if (err) throw err;
-          for(let i=0;i<result.length;i++){
-            primaryProperties += result[0][i];
-            primaryProperties +=", ";
-            primaryKey += result[1][i];
-            primaryKey +=", ";
-          }
-        }); /// 쿼리 순서가 걱정됨.
-        conn.query(sqlForCategory, function (err, result) {
-          if (err) throw err;
-          for(let i=0;i<result.length-1;i++){
-            primaryProperties += result[0][i];
-            primaryProperties +=", ";
-            primaryKey += result[1][i];
-            primaryKey +=", ";
-          }
-          primaryProperties += result[0][result.length-1];
-          primaryKey += result[1][result.length-1];
-          
-        });
-        var sql = "insert into scantable(대표_속성, 대표_결합키) VALUES (?,?)";
-        conn.query(sql, [primaryProperties,primaryKey],function (err, result) {
+        ar sqlForStatistic = 'insert into scantable(대표_속성, 대표_결합키) SELECT  GROUP_CONCAT(IFNULL(대표_속성,""),ifnull(CONCAT (",", 대표_결합키),"") SEPARATOR "," ) FROM statistic_attribute, category_attribute';
+        var sqlForCategory = 'insert into scantable(대표_속성, 대표_결합키) SELECT  GROUP_CONCAT(IFNULL(student_name,""),ifnull(CONCAT (",", candidate_key),"") SEPARATOR "," ) FROM statistic_attribute, category_attribute';
+        conn.query([sqlForStatistic,sqlForCategory], function (err, result) {
           if (err) throw err;
           console.log("Table created");
         });
