87f75aa4cf650f92afefa6b3cf85f9c90c0b7543
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2022-12-21 00:19:17 +0900

    downCSV업데이트-> csv파일 만들고 서버가 파일 다운로드시키는 것 구현.
    filepath는 csv가 만들어지는 위치로 맞춰주고, savedfilename은 테이블 이름으로 맞춰주면 됩니다

--- app.js
+++ app.js
@@ -272,12 +272,10 @@ app.get('/get/multiresult/:table1/:table2', (req,res)=>{
 
 //down csv
 var make_csv = require('./make_csv.js')
-app.get('/download/csv', (req,res)=>{
+app.get('/download/csv', (req,res)=>{   /// html에서 <a href="/downlaod/csv></a>로 하면 되는 것 같습니다
   make_csv.get("1_fitness_measurement")
+  var filepath ="C:/Users/"  // filepath는 csv파일 local에 저장된 위치 ex)C:/result/
+  var savedfilename ="1_fitness_measurement.csv"; //  name은 csv파일 이름  ex) abcd.csv
+  res.download(filepath+savedfilename);
 })
 
-app.get('/주소',async(req,res,next)=>{  /// 프론트 html에서 <a href = '/주소'>내용</a>형식으로 맞추면 됨.
-  var filepath ="C:/Users/dltkd/DB_Project/modulization/db-back-master/"  // filepath는 csv파일 local에 저장된 위치 ex)C:/result/
-  var savedfilename ="category_attribute.csv"; //  name은 csv파일 이름  ex) abcd.csv
-  res.download(filepath+savedfilename);
-});
