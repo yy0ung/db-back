89e36ba0103841aff7ec1062fcf480155506b085
2020147527 <yiji_kim@yonsei.ac.kr>
2022-12-20 15:56:22 +0900

    대표결합키의 속성 받아오기_미완

---
+++ key_att.js
@@ -0,0 +1,17 @@
+var con = require('./connect');
+var key_att = {};
+
+key_att.create = function (tablename){
+    con.getConnection(function(err,conn){
+        var sql = "SELECT `속성명` FROM scan_table WHERE `대표_결합키` IS NOT NULL;";
+        conn.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+        return ;
+    });
+    
+};
+
+
+module.exports = key_att;
